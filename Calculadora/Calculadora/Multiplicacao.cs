f36212cdfdec6b1075b65970be593c24335298d0
leon-08024 <cauecardosodefranca@gmail.com>
2025-08-18 20:47:55 -0300

    realização parcial do metodo de

--- Calculadora/Calculadora/Multiplicacao.cs
+++ Calculadora/Calculadora/Multiplicacao.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 
 namespace Calculadora
 {
-    internal class Multiplicacao
+    internal class Multiplicacao 
     {
 
-        public static void Multiplicar(decimal num1, decimal num2)
+        public static void Executar(decimal num1, decimal num2)
         {
+
+            decimal resultado = num1*num2;
+
+            Console.WriteLine($"O seu Resultado da Multiplicação de {num1} e {num2} é {resultado}");
 
         }
 
