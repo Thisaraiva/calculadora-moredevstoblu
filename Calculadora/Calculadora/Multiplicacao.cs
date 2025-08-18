73766d6e1ccf5f51095f32860829c0c60ef5e427
leon-08024 <cauecardosodefranca@gmail.com>
2025-08-18 20:34:40 -0300

    teste

---
+++ Calculadora/Calculadora/Multiplicacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Multiplicacao
+    {
+
+        public static void Multiplicar(decimal num1, decimal num2)
+        {
+
+        }
+
+    }
+}
