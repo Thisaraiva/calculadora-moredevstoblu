8cf95b3ef6a9892f0dc0d5deb031540a941498f8
MarcioDivonei <marcio230213@gmail.com>
2025-08-18 21:17:37 -0300

    Divisão

--- Calculadora/Calculadora/Classes/Divisao.cs
+++ Calculadora/Calculadora/Classes/Divisao.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Calculadora.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace Calculadora.Classes
 {
-    internal class Divisao
+    internal class Divisao : IOperacao
     {
+        public static void Dividir(decimal num1, decimal num2)
+        {
+            if (num1 == 0 || num2 == 0)
+            {
+                Console.WriteLine("Impossivel Dividir por 0");
+            }
+            else {
+                decimal resultado = num1 / num2;
+                Console.WriteLine($"O seu Resultado da divisão de {num1} e {num2} é {resultado}");
+            };
+
+            
+
+        }
     }
 }
