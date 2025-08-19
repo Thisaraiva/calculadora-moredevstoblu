fd0d04fa19a530ab896aa9e5e2575ff12d03e13d
Thisaraiva <thiagofreitassaraiva@yahoo.com.br>
2025-08-19 01:05:20 -0300

    Correção da classe Divisao

--- Calculadora/Calculadora/Classes/Divisao.cs
+++ Calculadora/Calculadora/Classes/Divisao.cs
@@ -5,12 +5,7 @@ namespace Calculadora.Classes
     public class Divisao : IOperacao
     {
         public decimal Calcular(decimal num1, decimal num2)
-        {
-            if (num2 == 0)
-            {
-                throw new DivideByZeroException("Divisão por zero não é permitida.");
-
-            }
+        {            
             return num1 / num2;
         }
     }
